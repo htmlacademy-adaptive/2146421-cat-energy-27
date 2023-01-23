42e9045cab3dd5238bd2780d839f62df1d0cd191
Kate Velikodnaya <emvelikodnaya@gmail.com>
2023-01-23 13:16:31 +0400

    final edits

---
+++ source/js/script-map.js
@@ -0,0 +1,7 @@
+let iframe = document.querySelector('.contact-information__iframe');
+let picture = document.querySelector('.contact-information__picture');
+
+iframe.classList.remove('contact-information__iframe--closed');
+iframe.classList.add('contact-information__iframe--opened');
+
+picture.classList.add('contact-information__picture--closed');
